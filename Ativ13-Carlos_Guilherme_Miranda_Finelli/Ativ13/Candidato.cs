d2cdcc6fde2606312785592d0d83a4ecf626f799
Carlos Guilherme Miranda Finelli <60704748+CarlosFinelli@users.noreply.github.com>
2020-10-30 17:29:17 -0300

    Conclusão atividade 13

---
+++ Ativ13-Carlos_Guilherme_Miranda_Finelli/Ativ13/Candidato.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ativ13
+{
+    class Candidato
+    {
+        public String nome, Partido, Profissao, CargoP;
+        public double Patrimonio;
+
+        public readonly String Municipio = "Lagoa Santa";
+        public readonly String Ficha = "Limpa";
+
+        public Candidato(String nome, String partido, String profissao, String cargoP, double patrimonio)
+        {
+            this.nome = nome;
+            Partido = partido;
+            Profissao = profissao;
+            CargoP = cargoP;
+            Patrimonio = patrimonio;
+        }
+        public override string ToString()
+        {
+            return $"Candidato {nome} cadastrado com sucesso!\n" +
+                $"Dados da conferência:\nCandidato a prefeito de {Municipio}\n" +
+                $"Nome: {nome}\nPartido: {Partido}\nPatrimônio declarado: {Math.Round(Patrimonio, 2)}\n" +
+                $"Profissão: {Profissao}\nFicha limpa: {Ficha} ";
+        }
+    }
+}
---
+++ Ativ13-Carlos_Guilherme_Miranda_Finelli/Ativ13/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ativ13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual o nome: ");
+            String Nome = Console.ReadLine();
+            Console.Write("Partido: ");
+            String Partido = Console.ReadLine();
+            Console.Write("Patrimônio declarado: ");
+            double Patrimonio = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Qual a atual profissão: ");
+            String Profissao = Console.ReadLine();
+            Console.Write("Cargo pretendido: ");
+            String CargoP = Console.ReadLine();
+            Console.Clear();
+            Candidato candidato = new Candidato(Nome, Partido, Profissao, CargoP, Patrimonio);
+            Console.WriteLine(candidato);
+        }
+    }
+}
